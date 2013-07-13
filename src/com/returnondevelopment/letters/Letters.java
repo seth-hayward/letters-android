0007600486df646a4c72da6f1e95d2646032a1e7
Seth Hayward <seth.hayward@gmail.com>
2013-07-12 17:36:21 -0700

    using a different kind of viewpager that doesn't initialize pages until they are visible

--- src/com/returnondevelopment/letters/Letters.java
+++ src/com/returnondevelopment/letters/Letters.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
@@ -31,36 +32,37 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener {
 	 * intensive, it may be best to switch to a
 	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
 	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+//	SectionsPagerAdapter mSectionsPagerAdapter;
 
+	static final int ITEMS = 10;
+	MyAdapter mAdapter;
+	ViewPager mPager;
+	
 	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
-	ViewPager mViewPager;
+//	ViewPager mViewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_letters);
 
+		mAdapter = new MyAdapter(getSupportFragmentManager());		
+		mPager = (ViewPager)findViewById(R.id.pager);
+		mPager.setAdapter(mAdapter);
+		
+		
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+		
+		
+//		// When swiping between different sections, select the corresponding
+//		// tab. We can also use ActionBar.Tab#select() to do this if we have
+//		// a reference to the Tab.
+		mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 					@Override
 					public void onPageSelected(int position) {
 						actionBar.setSelectedNavigationItem(position);
@@ -68,17 +70,35 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener {
 				});
 
 		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+		for (int i = 0; i < 5; i++) {
 			// Create a tab with text corresponding to the page title defined by
 			// the adapter. Also specify this Activity object, which implements
 			// the TabListener interface, as the callback (listener) for when
 			// this tab is selected.
 			actionBar.addTab(actionBar.newTab()
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
+					.setText(getPageTitle(i))
 					.setTabListener(this));
 		}
 	}
 
+
+	public String getPageTitle(int position) {
+		Locale l = Locale.getDefault();
+		switch (position) {
+		case 0:
+			return getString(R.string.title_home).toUpperCase(l);
+		case 1:
+			return getString(R.string.title_more).toUpperCase(l);
+		case 2:
+			return getString(R.string.title_bookmarks).toUpperCase(l);
+		case 3:
+			return getString(R.string.title_search).toUpperCase(l);
+		case 4:
+			return getString(R.string.title_send).toUpperCase(l);			
+		}
+		return null;
+	}			
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -91,7 +111,7 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener {
 			FragmentTransaction fragmentTransaction) {
 		// When the given tab is selected, switch to the corresponding page in
 		// the ViewPager.
-		mViewPager.setCurrentItem(tab.getPosition());
+		mPager.setCurrentItem(tab.getPosition());
 	}
 
 	@Override
@@ -104,6 +124,28 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener {
 			FragmentTransaction fragmentTransaction) {
 	}
 
+	
+	public static class MyAdapter extends FragmentStatePagerAdapter {
+		public MyAdapter(FragmentManager fragmentManager) {
+			super(fragmentManager);
+		}
+		
+		@Override
+		public int getCount() {
+			return ITEMS;			
+		}
+		
+		@Override
+		public Fragment getItem(int position) {
+			Fragment fragment = new DummySectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+			fragment.setArguments(args);									
+			return fragment;
+		}
+						
+	}
+	
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
@@ -205,7 +247,25 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener {
 						
 			return rootView;
 		}
-		
+
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+			case 0:
+				return getString(R.string.title_home).toUpperCase(l);
+			case 1:
+				return getString(R.string.title_more).toUpperCase(l);
+			case 2:
+				return getString(R.string.title_bookmarks).toUpperCase(l);
+			case 3:
+				return getString(R.string.title_search).toUpperCase(l);
+			case 4:
+				return getString(R.string.title_send).toUpperCase(l);
+				
+			}
+			return null;
+		}		
+				
 
 	    private class Callback extends WebViewClient{ 
 
