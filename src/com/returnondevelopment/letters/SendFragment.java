57eaa22e71112f86c84f43b670b04d997ff793b8
Seth Hayward <seth.hayward@gmail.com>
2013-07-16 11:26:27 -0700

    binding the send button

--- src/com/returnondevelopment/letters/SendFragment.java
+++ src/com/returnondevelopment/letters/SendFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SendFragment extends Fragment {
-		
+	
+	Button button_Send;
+	
 	public SendFragment() {
 		
 		
@@ -19,8 +23,24 @@ public class SendFragment extends Fragment {
         // Inflate the layout for this fragment
     	
     	View sendView = inflater.inflate(R.layout.fragment_send, container, false);
+
+		button_Send = (Button)sendView.findViewById(R.id.btn_send);
+		 
+		button_Send.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+
+				// read the response from the server
+				// display the letter page... 
+ 
+			}
+ 
+		});
+ 
+    	
     	
     	return sendView;
-    }
-
+    }    
+		 
 }
