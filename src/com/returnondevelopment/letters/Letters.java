68d5e7705c2e2cb2312de40a80f26d12573d226d
Seth Hayward <seth.hayward@gmail.com>
2013-07-15 17:23:06 -0700

    remove dead code

--- src/com/returnondevelopment/letters/Letters.java
+++ src/com/returnondevelopment/letters/Letters.java
@@ -72,16 +72,7 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener, 
 						mAdapter = new MyAdapter(getSupportFragmentManager(), current_page);		
 						mPager = (ViewPager)findViewById(R.id.pager);
 						mPager.setAdapter(mAdapter);
-						
-						mPager.setCurrentItem(0);
-								
-						// Set up the action bar.						
-						
-						View rootView = mPager.getRootView();		
-						WebView dummyWebView = (WebView) rootView.findViewById(R.id.section_webView);
-												
-						current_page_number = 1;
-																		
+																																
 					    Log.d("CurrentPage", current_page + " - " + Integer.toString(spinner_position));
 
 					    return true;
