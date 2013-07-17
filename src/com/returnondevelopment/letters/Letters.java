9279d169eeac31f17db391ca1edaf330000df3ce
Seth Hayward <seth.hayward@gmail.com>
2013-07-17 12:51:25 -0700

    adding google analytics integration code

--- src/com/returnondevelopment/letters/Letters.java
+++ src/com/returnondevelopment/letters/Letters.java
@@ -140,5 +140,16 @@ public class Letters extends FragmentActivity implements ActionBar.TabListener, 
 		
 	}
 
-		
+	  @Override
+	  public void onStart() {
+	    super.onStart();
+	    EasyTracker.getInstance().activityStart(this); // Add this method.
+	  }
+
+	  @Override
+	  public void onStop() {
+	    super.onStop();
+	    EasyTracker.getInstance().activityStop(this); // Add this method.
+	  }
+	
 }
