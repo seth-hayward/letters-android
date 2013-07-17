d0c74e11bcd67877e0ffc1425490f994222b890d
Seth Hayward <seth.hayward@gmail.com>
2013-07-16 18:19:57 -0700

    moved every thing to an async task, was working now its not, tears

--- src/com/returnondevelopment/letters/SendFragment.java
+++ src/com/returnondevelopment/letters/SendFragment.java
@@ -2,7 +2,9 @@ package com.returnondevelopment.letters;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +12,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -25,6 +28,7 @@ import android.R.bool;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -77,99 +81,14 @@ public class SendFragment extends Fragment {
 		button_Send = (Button)sendView.findViewById(R.id.btn_send);
 		text_Letter = (EditText)sendView.findViewById(R.id.text_Letter);
 		button_Send.setOnClickListener(new OnClickListener() {
- 
+
 			@Override
 			public void onClick(View arg0) {
 
-				// read the response from the server
-				// display the letter page... 
-			    // Create a new HttpClient and Post Header
-			    HttpClient httpclient = new DefaultHttpClient();
-			    HttpPost httppost = new HttpPost("http://ww.letterstocrushes.com/home/mail");
-		    	AlertDialog errorDialog = new AlertDialog.Builder(getActivity()).create();
-		    	AlertDialog successDialog = new AlertDialog.Builder(getActivity()).create();
-
-		    	Log.d("status", "onClick: " + text_Letter.getText().toString());
-			    
-			    try {
-			        // Add your data
-			    	String letter_message = text_Letter.getText().toString();
-			    	
-			        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-			        nameValuePairs.add(new BasicNameValuePair("letterText", letter_message));
-			        nameValuePairs.add(new BasicNameValuePair("letterCountry", "US"));
-			        nameValuePairs.add(new BasicNameValuePair("mobile", "1"));
-			        
-			        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-			        Log.d("status", "before execute");
-			        
-			        // Execute HTTP Post Request
-			        HttpResponse response = httpclient.execute(httppost);
-
-			        Log.d("status", "before reader");
-
-			        
-			        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-			        StringBuilder builder = new StringBuilder();
-			        for (String line = null; (line = reader.readLine()) != null;) {
-			            builder.append(line).append("\n");
-			        }
-			        JSONTokener tokener = new JSONTokener(builder.toString());
-			        JSONArray finalResult = new JSONArray(tokener);
-
-			        // this is awful
-			        int new_letter_id = 0;
-			        boolean success = false;
-
-			        Log.d("status", "before call");
-			        
-			        for(int i = 0; i < finalResult.length(); i++) {
-			        	
-			        	JSONObject row = finalResult.getJSONObject(i);
-				        Log.d("status", "get object");
-		        	
-			        	int server_response = row.getInt("response");
-			        	final String server_message = row.getString("message");
-			        	String server_guid = row.getString("guid");
-			        	
-			        	if(server_response == 1) {
-					        Log.d("status", "success");
-
-			        		successDialog.setTitle("error");
-			            	successDialog.setMessage(server_message);
-			            	successDialog.setButton("Close", new DialogInterface.OnClickListener() {
-			        	    	public void onClick(DialogInterface dialog, int which) {
-			    					mCallback.onLetterSend(Integer.parseInt(server_message));			        	    		
-			        	    	}
-			            	});
-			        		
-			        	} else {
-			        		
-			            	errorDialog.setTitle("error");
-			            	errorDialog.setMessage(server_message);
-			            	errorDialog.setButton("Close", new DialogInterface.OnClickListener() {
-			        	    	public void onClick(DialogInterface dialog, int which) {
-			        	    	// here you can add functions
-			        	    		Log.d("ok", "OK");
-			        	    	}
-			            	});
-			            	errorDialog.show();
-			            	
-			        		
-			        	}
-			        	
-			        	
-			        }
-			        			        				        			        
-			    } catch (ClientProtocolException e) {
-			        // TODO Auto-generated catch block			    	
-			    } catch (IOException e) {
-			        // TODO Auto-generated catch block
-			    } catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}				
+				DownloadWebPageTask task = new DownloadWebPageTask();
+			    task.execute(new String[] { "http://www.vogella.de" });		    
+				
+				
 				 
 			}
  
@@ -179,5 +98,88 @@ public class SendFragment extends Fragment {
     	
     	return sendView;
     }    
+    
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+          String response = "";
+          for (String url : urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            
+
+	    	AlertDialog errorDialog = new AlertDialog.Builder(getActivity()).create();
+	    	AlertDialog successDialog = new AlertDialog.Builder(getActivity()).create();            
+            
+	        // Add your data
+	    	String letter_message = text_Letter.getText().toString();
+	    	
+	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+	        nameValuePairs.add(new BasicNameValuePair("letterText", letter_message));
+	        nameValuePairs.add(new BasicNameValuePair("letterCountry", "US"));
+	        nameValuePairs.add(new BasicNameValuePair("mobile", "1"));
+
+		    HttpPost httppost = new HttpPost("http://www.letterstocrushes.com/home/mail");
+	        try {
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+                      
+            try {
+              HttpResponse execute = client.execute(httppost);
+              
+		        Log.d("status", "before reader");
+
+
+		        InputStream content = execute.getEntity().getContent();
+
+		          BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+		          String s = "";
+		          while ((s = buffer.readLine()) != null) {
+		            response += s;
+		          }		        
+		        		          
+//		        BufferedReader reader = new BufferedReader(new InputStreamReader(execute.getEntity().getContent(), "UTF-8"));
+//		        StringBuilder builder = new StringBuilder();
+//		        for (String line = null; (line = reader.readLine()) != null;) {
+//		        	builder.append(line).append("\n");
+//		        }
+//		        JSONTokener tokener = new JSONTokener(builder.toString());
+//		        JSONArray finalResult = new JSONArray(tokener);
+//
+//		        return builder.toString();
+		        
+//		        // this is awful
+//		        int new_letter_id = 0;
+//		        boolean success = false;
+//
+//		        Log.d("status", "before call");
+//		        
+//		        for(int i = 0; i < finalResult.length(); i++) {
+//		        	
+//		        	JSONObject row = finalResult.getJSONObject(i);
+//			        Log.d("status", "get object");
+//	        	
+//		        	int server_response = row.getInt("response");
+//		        	final String server_message = row.getString("message");
+//		        	String server_guid = row.getString("guid");
+//
+//		        	response = server_message;
+//		        	
+//		        }
+
+            } catch (Exception e) {
+              e.printStackTrace();
+              text_Letter.setText(e.getMessage());
+            }
+          }
+          return response;
+        }
+
+        protected void onPostExecute(String result) {
+          text_Letter.setText(result);
+        }
+      }
+    
 		 
 }
